8262033d1ec23880689be6a972499c3ad577bfe7
Cassano <ninocb@gmail.com>
2021-11-14 02:26:33 -0300

    Primeiro Commit Real
    Adicionando index.js e client.js com algumas informações sobre o bot e realizado testes iniciais no Discord.

---
+++ index.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+const Client = require('./src/structures/Client')
+
+const client = new Client({
+    intents: [
+        'GUILDS',
+        'GUILD_MESSAGE_REACTIONS',
+        'GUILD_MESSAGES',
+        'GUILD_INVITES',
+        'GUILD_VOICE_STATES',
+        'GUILD_MEMBERS',
+        'GUILD_PRESENCES'
+    ]
+})
+
+client.once('ready', function() {
+    console.log('Bot iniciado!')
+})
+/* Testes iniciais com o Bot R2D2 */
+client.on('messageCreate', function (message) {
+    if (message.content === "Oi!"){
+        message.reply('Olá, tudo bem?')
+    }
+    console.log(message)
+})
+
+client.login(process.env.BOT_TOKEN)
