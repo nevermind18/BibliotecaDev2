b0002778d2c96ac466898d184f6dec51eba06d95
Cassano <ninocb@gmail.com>
2021-12-21 10:12:46 -0300

    Atualização index.js
    Atualizado código onde se relaciona com o Client e faz toda interação inicial do bot.

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
-const Client = require('./src/client')
+require("dotenv").config()
+
+const Client = require('./src/structures/Client')
 
 const client = new Client({
-    intents: [
+    intents:[
         'GUILDS',
         'GUILD_MESSAGE_REACTIONS',
         'GUILD_MESSAGES',
         'GUILD_INVITES',
         'GUILD_VOICE_STATES',
         'GUILD_MEMBERS',
-        'GUILD_PRESENCES'
+        'GUILD_PRESENCES'  
     ]
 })
 
-client.login(BOT_TOKEN);
+/*
+client.once('ready', function() {
+    console.log('Bot logado!')
+})
+
+client.on('messageCreate', function (message) {
+    if (message.content === "Oi!"){
+        message.reply('Olá, tudo bem?')
+    }
+    //console.log(message)
+})
+*/
+
+client.login(process.env.BOT_TOKEN)
+
